419ccd59de9f7b22f25cfda6d09eb7aa20c284d7
Philip Arvidsson <philip@philiparvidsson.com>
2017-04-05 19:59:21 +0200

    initial stuff

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Sap6-Game
 
 *More info at [Sap6—Agenda](https://paper.dropbox.com/doc/Sap6Agenda-IVHtFon27xPOxeDWtnWZs).*
+
+## Building the project
+
+### Linux
+
+Run `make` in the project root directory.
+
+### macOS
+
+Run `make` in the project root directory.
+
+### Windows
+
+n/a
---
+++ Source/Program.cs
@@ -0,0 +1,26 @@
+namespace Sap6 {
+
+/*--------------------------------------
+ * USINGS
+ *------------------------------------*/
+
+using System;
+
+/*--------------------------------------
+ * CLASSES
+ *------------------------------------*/
+
+/// <summary>Provides a program entry point.</summary>
+public static class Program {
+    /*--------------------------------------
+     * PRIVATE METHODS
+     *------------------------------------*/
+
+    /// <summary>Program entry point.</summary>
+    /// <param name="args">The command line arguments.</param>
+    private static void Main(string[] args) {
+        Console.WriteLine("Hello. :-)");
+    }
+}
+
+}
