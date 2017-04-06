bc3b51575a61e61cbc005a27aef685d219d9beb8
Philip Arvidsson <philip@philiparvidsson.com>
2017-04-06 20:25:26 +0200

    working on ecs boilerplate

---
+++ Source/Base/Component.cs
@@ -0,0 +1,12 @@
+namespace Sap6.Base {
+
+/*--------------------------------------
+ * CLASSES
+ *------------------------------------*/
+
+/// <summary>Represents an entity component.</summary>
+public abstract class Component {
+    // Could also be an interface (IComponent).
+}
+
+}
---
+++ Source/Base/Entity.cs
@@ -0,0 +1,71 @@
+namespace Sap6.Base {
+
+/*--------------------------------------
+ * USINGS
+ *------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+
+/*--------------------------------------
+ * CLASSES
+ *------------------------------------*/
+
+/// <summary>Represents an entity.</summary>
+public sealed class Entity {
+    /*--------------------------------------
+     * PRIVATE FIELDS
+     *------------------------------------*/
+
+    /// <summary>The attached components.</summary>
+    private readonly Dictionary<Type, Component> m_Components =
+        new Dictionary<Type, Component>();
+
+    /*--------------------------------------
+     * PUBLIC METHODS
+     *------------------------------------*/
+
+    /// <summary>Adds a component to the entity.</summary>
+    /// <param name="component">The component to add to the entity.</param>
+    public void AddComponent(Component component) {
+        m_Components.Add(component.GetType(), component);
+    }
+
+    /// <summary>Checks whether the entity has a component of the specified
+    ///          type.</summary>
+    /// <param name="type">The type of the component to look for.</param>
+    /// <returns><see langword="true"/> if the entity has a component of the
+    ///          specified type.</returns>
+    public bool HasComponent(Type type) {
+        return m_Components.ContainsKey(type);
+    }
+
+    /// <summary>Checks whether the entity has a component of the specified
+    ///          type.</summary>
+    /// <typeparam name="T">The type of the component to look for.</typeparam>
+    /// <returns><see langword="true"/> if the entity has a component of the
+    ///          specified type.</returns>
+    public bool HasComponent<T>() where T: Component {
+        return HasComponent(typeof (T));
+    }
+
+    /// <summary>Removes the component of the specified type.</summary>
+    /// <param name="component">The type of the component to remove from the
+    ///                         entity.</param>
+    /// <returns><see langword="true"/> if a component of the specified type was
+    ///          removed from the entity.</returns>
+    public bool RemoveComponent(Type type) {
+        return m_Components.Remove(type);
+    }
+
+    /// <summary>Removes the component of the specified type.</summary>
+    /// <typeparam name="T">The type of the component to remove from the
+    ///                         entity.</typeparam>
+    /// <returns><see langword="true"/> if a component of the specified type was
+    ///          removed from the entity.</returns>
+    public bool RemoveComponent<T>() where T: Component {
+        return RemoveComponent(typeof (T));
+    }
+}
+
+}
