2ee2e98199817a7663c8256539bdc08b2a9350d2
crippa9 <crippa9@hotmail.com>
2017-04-25 21:50:58 +0200

    Added pipeline project, started building map creation processor

---
+++ ContentPipeline/TerrainImporter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+using TInput = Microsoft.Xna.Framework.Color;
+using Microsoft.Xna.Framework;
+
+namespace ContentPipeline {
+    /// <summary>
+    /// This class will be instantiated by the XNA Framework Content Pipeline
+    /// to import a file from disk into the specified type, TImport.
+    ///
+    /// This should be part of a Content Pipeline Extension Library project.
+    ///
+    /// TODO: change the ContentImporter attribute to specify the correct file
+    /// extension, display name, and default processor for this importer.
+    /// </summary>
+
+    [ContentImporter(".png", DisplayName = "Terrain Importer", DefaultProcessor = "")]
+    public class TerrainImporter : ContentImporter<TInput> {
+
+        public override TInput Import(string filename, ContentImporterContext context) {
+
+            // TODO: process the input object, and return the modified data.
+            context.Logger.LogMessage("bajs" + filename);
+            return Color.Black;
+        }
+
+    }
+
+}
---
+++ ContentPipeline/TerrainProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+
+using TInput = Microsoft.Xna.Framework.Content.Pipeline.Graphics.TextureContent;
+using TOutput = Microsoft.Xna.Framework.Content.Pipeline.Graphics.NodeContent;
+
+namespace ContentPipeline {
+    [ContentProcessor(DisplayName = "SAP6 - Terrain Processor")]
+    public class TerrainProcessor : ContentProcessor<TInput, TOutput> {
+        public override TOutput Process(TInput input, ContentProcessorContext context) {
+            try {
+                input.ConvertBitmapType(typeof(PixelBitmapContent<Color>));
+            }
+            catch (Exception ex) {
+                context.Logger.LogImportantMessage("Could not convert input texture for processing. " + ex.ToString());
+                throw ex;
+            }
+            var bmp = (PixelBitmapContent<Color>)input.Faces[0][0];
+            var image = new Color[bmp.Height][];
+            for (var y = 0; y < bmp.Height; y++) {
+                image[y] = bmp.GetRow(y);
+            }
+            PositionCollection pc = new PositionCollection(); pc.Add()
+            MeshContent mc = new MeshContent() {  }
+            GeometryContent g = new GeometryContent() {  }
+
+            context.Logger.LogMessage("test");
+            return new TOutput();
+        }
+    }
+}
