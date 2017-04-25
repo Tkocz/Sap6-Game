56240174f0d8b74e099532f2bcbc956db54f1968
Philip Arvidsson <philip@philiparvidsson.com>
2017-04-25 07:27:32 +0200

    comments for clarity + some refactoring

--- Source/GameName/Dev/CollTestScene1.cs
+++ Source/GameName/Dev/CollTestScene1.cs
@@ -10,24 +10,36 @@ using EngineName.Systems;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
+// NOTE: Do not change anything here. Rather, create a copy of the scene and modify the copy. We
+//       want to keep test cases consistent!
+/// <summary>Provides a simple test case for collisions. Running it, you should see four spheres
+//           colliding on the screen in a sane manner.</summary>
 public sealed class CollTestScene1: Scene {
     public override void Init() {
-        AddSystems(new FpsCounterSystem(updatesPerSec: 10),
-                   new RenderingSystem(),
-                   new CameraSystem(),
-                   new PhysicsSystem());
+        AddSystems(new    PhysicsSystem(),
+                   new     CameraSystem(),
+                   new  RenderingSystem(),
+                   new FpsCounterSystem(updatesPerSec: 10));
 
         base.Init();
 
         InitCam();
 
-        CreateBall(new Vector3(-3.5f, 0.0f, 0.0f), new Vector3( 1.0f, 0.0f, 0.0f));
-        CreateBall(new Vector3( 3.5f, 1.0f, 0.0f), new Vector3(-1.0f, 0.0f, 0.0f));
+        //                  -- position --                  -- velocity --
+        CreateBall(new Vector3(-3.5f,  0.0f, 0.0f), new Vector3( 1.0f,  0.0f, 0.0f));
+        CreateBall(new Vector3( 3.5f,  1.0f, 0.0f), new Vector3(-1.0f,  0.0f, 0.0f));
         CreateBall(new Vector3( 3.5f, 14.0f, 0.0f), new Vector3(-1.0f, -3.0f, 0.0f));
-        CreateBall(new Vector3( -3.5f, 14.0f, 0.0f), new Vector3(0.5f, -4.0f, 0.0f));
+        CreateBall(new Vector3(-3.5f, 14.0f, 0.0f), new Vector3( 0.5f, -4.0f, 0.0f));
     }
 
-    private int CreateBall(Vector3 p, Vector3 v) {
+    /// <summary>Creates a new ball in the scene with the given position and velocity.</summary>
+    /// <param name="p">The ball position, in world-space.</param>
+    /// <param name="v">The initial velocity to give to the ball.</param>
+    /// <param name="r">The ball radius.</param>
+    private int CreateBall(Vector3 p, Vector3 v, float r=1.0f) {
+        // TODO: Radius is unsupported. Scale ball by radius *and make sure to implement support in
+        // the physics system*.
+
         var ball = AddEntity();
 
         AddComponent(ball, new CBody {
@@ -49,19 +61,19 @@ public sealed class CollTestScene1: Scene {
         return ball;
     }
 
-    private int InitCam(float fov=90.0f) {
-        // Convert to rads
-        fov *= (float)Math.PI/180.0f;
-
-        var zNear = 0.01f;
-        var zFar  = 100.0f;
-
-        var ar  = Game1.Inst.GraphicsDevice.Viewport.AspectRatio;
-        var cam = AddEntity();
+    /// <summary>Sets up the camera.</summary>
+    /// <param name="fovDeg">The camera field of view, in degrees.</param>
+    /// <param name="zNear">The Z-near clip plane, in meters from the camera.</param>
+    /// <param name="zFar">The Z-far clip plane, in meters from the camera..</param>
+    private int InitCam(float fovDeg=90.0f, float zNear=0.01f, float zFar=100.0f) {
+        var aspect = Game1.Inst.GraphicsDevice.Viewport.AspectRatio;
+        var cam    = AddEntity();
+        var fovRad = fovDeg*2.0f*(float)Math.PI/360.0f;
+        var proj   = Matrix.CreatePerspectiveFieldOfView(fovRad, aspect, zNear, zFar);
 
         AddComponent(cam, new CCamera {
-            Projection     = Matrix.CreatePerspectiveFieldOfView(fov, ar, zNear, zFar),
-            ClipProjection = Matrix.CreatePerspectiveFieldOfView(fov, ar, zNear, zFar)
+            ClipProjection = proj,
+            Projection     = proj,
         });
 
         AddComponent(cam, new CTransform {
