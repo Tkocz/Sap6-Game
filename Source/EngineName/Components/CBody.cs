e97dbe8591906a6eb6d2c38fb7796d02d62b7bb7
Philip Arvidsson <philip@philiparvidsson.com>
2017-04-25 10:45:42 +0200

    added phil entry point for easier dev

--- Source/EngineName/Components/CBody.cs
+++ Source/EngineName/Components/CBody.cs
@@ -8,8 +8,14 @@ using Microsoft.Xna.Framework;
 /// <summary>Provides a physical representation-component for simulating physical effects on
 /// entities in the game world.</summary>
 public sealed class CBody: EcsComponent {
-    /// <summary>The axis-aligned bounding box.</summary>
+    /// <summary>The axis-aligned bounding box, used for coarse-phase collision detection.</summary>
     public BoundingBox Aabb;
+
+    /// <summary>The body sphere radius. Currently, the physics system only supports sphere-sphere
+    ///          collisions detection and resolution. During fine-phase collision detection, the
+    ///          radius is used to solve the collision between two bodies as if they were
+    ///          sphere-shaped.</summary>
+    public float Radius = 1.0f;
 
     /// <summary>The scalar inverse of the mass, in kilograms.</summary>
     public float InvMass = 1.0f;
