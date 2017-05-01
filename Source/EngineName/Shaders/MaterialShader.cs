f132c75fa9023c3b1d080b02cddc3a496ed54a7a
Philip Arvidsson <philip@philiparvidsson.com>
2017-05-01 20:50:30 +0200

    comments for clarity

--- Source/EngineName/Shaders/MaterialShader.cs
+++ Source/EngineName/Shaders/MaterialShader.cs
@@ -1,20 +1,49 @@
 namespace EngineName.Shaders {
 
+//--------------------------------------
+// USINGS
+//--------------------------------------
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
+//--------------------------------------
+// CLASSES
+//--------------------------------------
+
+/// <summary>Represents a material shader used to render objects in the world.</summary>
 public class MaterialShader {
+
+    //--------------------------------------
+    // NON-PUBLIC FIELDS
+    //--------------------------------------
+
+    /// <summary>The effect used to render the material in the GPU.</summary>
     internal readonly Effect mEffect;
 
+    /// <summary>The model matrix.</summary>
     private Matrix mModel;
+
+    /// <summary>The model matrix shader parameter.</summary>
     private EffectParameter mModelParam;
 
+    /// <summary>The projection matrix.</summary>
     private Matrix mProj;
+
+    /// <summary>The projection matrix shader parameter.</summary>
     private EffectParameter mProjParam;
 
+    /// <summary>The view matrix.</summary>
     private Matrix mView;
+
+    /// <summary>The view matrix shader parameter.</summary>
     private EffectParameter mViewParam;
 
+    //--------------------------------------
+    // PUBLIC PROPERTIES
+    //--------------------------------------
+
+    /// <summary>Gets or sets the model matrix.</summary>
     public Matrix Model {
         get {
             return mModel;
@@ -26,6 +55,7 @@ public class MaterialShader {
         }
     }
 
+    /// <summary>Gets or sets the projection matrix.</summary>
     public Matrix Proj {
         get {
             return mProj;
@@ -37,6 +67,7 @@ public class MaterialShader {
         }
     }
 
+    /// <summary>Gets or sets the view matrix.</summary>
     public Matrix View {
         get {
             return mView;
@@ -48,7 +79,12 @@ public class MaterialShader {
         }
     }
 
+    //--------------------------------------
+    // PUBLIC CONSTRUCTORS
+    //--------------------------------------
 
+    /// <summary>Initializes a new material shader.</summary>
+    /// <param name="effect">The underlying shader.</param>
     public MaterialShader(Effect effect) {
         mEffect = effect;
 
