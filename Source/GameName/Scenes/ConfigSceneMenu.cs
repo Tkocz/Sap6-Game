1afd2450e12ee99e76fe217fda95d03a18bbf3d8
Philip Arvidsson <philip@philiparvidsson.com>
2017-06-01 11:45:34 +0200

    see #130

--- Source/GameName/Scenes/ConfigSceneMenu.cs
+++ Source/GameName/Scenes/ConfigSceneMenu.cs
@@ -97,7 +97,6 @@ namespace GameName.Scenes {
             {
                 addWatingForPlayers();
                 OnEvent("update_peers", updatePeers);
-                OnEvent("selchanged", data => SfxUtil.PlaySound("Sounds/Effects/Click"));
             }
             else
             {
@@ -105,6 +104,8 @@ namespace GameName.Scenes {
                 SfxUtil.PlayMusic("Sounds/Music/MainMenu");
                 CreateLabels();
             }
+
+            OnEvent("selchanged", data => SfxUtil.PlaySound("Sounds/Effects/Click"));
 
         }
 
